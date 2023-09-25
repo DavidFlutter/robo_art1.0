de7ac4a042dadc730f150d829ff18e4336f9761b
DavidFlutter <david.haastrup@stu.cu.edu.ng>
2023-09-25 15:25:53 +0000

    Hero section design completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      
+      <HeroSection></HeroSection>
     </div>
   );
 }
---
+++ src/components/HeroSection.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className="HeroSection">
+        <img src="https://images.unsplash.com/photo-1563207153-f403bf289096?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cm9ib3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="wall E" />
+        <div className="search-tile">
+            <h2>Search for an artist</h2>
+            <p>
+                The internet's source for visuals. Powered by creators everywhere.
+            </p>
+            <form>
+                <input type="search" />
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
